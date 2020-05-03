77c52b72bc4f9b7c8f4a916d6266c42ccf110988
gdcs <gdcs97@gmail.com>
2020-05-03 22:47:58 +0200

    Added my share of ppt and readme and some models

--- README.md
+++ README.md
@@ -4,8 +4,6 @@ Gépi tanulás projekt beadandó 2020.
 ## Fájlok elhelyezkedése
 Hogyan lehet reprodukálni a fájlokat.
 
-### Géczi Dániel munkája:
-
 ### Heinc Emília munkája:
 #### Adatok feldolgozása:
   1. Először a beolvasas2.py-t kell futtatni. Ez kigenerálja a 12 hónapot a summed_data2.csv-be
@@ -15,6 +13,16 @@ Hogyan lehet reprodukálni a fájlokat.
 #### Ensemble model:
   1. Minden az Ensemble modellel kapcsolatban az **EnsembleModels.py**-ban találhatóak meg. Ezt a fájlt futtatva tanítattam be a modelleimet. Fontos, hogy több óra hosszát is elfut 1 modell tanítattása ezért a kapot betanított modellek adatait kimentettem .pkl fájlokba.
   2. A .pkl fájlok eredményeit az **osszesitett.ipynb**-ban jelenítettem meg és adtam rájuk magyarázatot. Saját gépen nem fog futni mert előtte fel kellett mountolni a meghajtót és a gépi tanulások mappában lévő .pkl fájlokra futtattam le a megjelenítést: https://drive.google.com/drive/folders/1XOYR1u01MvVn4v5ag2cK60basoCsKDmU?usp=sharing
+### Baseline taníttatása:
+  1. A **baseline_model.py**-ban a tanitas() fv-t kell meghívni a különböző baseline modellek előállításához, ezeket a fv. lementi a módszernevével ellátott pkl kiterjesztésű fájlokba.
+
+### Géczi Dániel munkája:
+### Dimenzió redukció
+A **dimension_reduction.py**-t kell futtatni és az elvégzi a dimenzió redukciót a **summed_data_to_train.csv**-n és lementi az eredményeket a modellek számára.
+A redukció SingleValueDecomposition (SVD)-vel és Principle Component Analysis (PCA)-val történik.
+
+  1. A **model_training.py**-ban található a DecisionTree (DT) model és a SupportVectorRegression (SVR) model és a redukált táblán tanított változataik, a fájl futtatásá értelemszerűen sorban elvégzi a modellek tanítását, de fontos tudni, hogy az SVR modellek
+     tanítása nagyon hosszú időt vesz igénybe, mivel paraméter opt. is fel van használva GridSearchCV-t alkalmazva.
   
 ## Diasor:
 bemutato.ppt-t fogjuk bemutatni a bemutatás pont alatt.
--- model_training.py
+++ model_training.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.model_selection import GridSearchCV
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error as MSE
+import pickle
 
 x_train = pd.read_csv('./files/x_train.csv')
 y_train = pd.read_csv('./files/y_train.csv')
@@ -10,7 +11,7 @@ x_test = pd.read_csv('./files/x_test.csv')
 y_test = pd.read_csv('./files/y_test.csv')
 
 def make_and_save_decision_tree_model():
-    dt = GridSearchCV(DecisionTreeRegressor(), {'max_depth': [4,5,6,7], 'min_samples_leaf': [0.05, 0.1, 0.15, 0.2, 0.3] }, cv=5, return_train_score=False)
+    dt = GridSearchCV(DecisionTreeRegressor(), {'max_depth': [4,5,6,7], 'min_samples_leaf': [0.05, 0.1, 0.15, 0.2, 0.3] }, cv=5, return_train_score=True)
     dt.fit(x_train, y_train.values.ravel())
     y_pred = dt.predict(x_test)
 
@@ -28,7 +29,7 @@ def make_and_save_decision_tree_model():
     pd.DataFrame(dt.cv_results_).to_pickle('./decision_tree_model.pkl')
 
 def make_svr_model():
-    model = GridSearchCV(SVC(), {'kernel': ['linear', 'poly', 'rbf', 'sigmoid'], 'C': [1,2,3,4,5,6,7,8,9,10]}, cv=5, return_train_score=False)
+    model = GridSearchCV(SVC(), {'kernel': ['linear', 'poly', 'rbf', 'sigmoid'], 'C': [1,2,3,4,5,6,7,8,9,10]}, cv=5, return_train_score=True)
     model.fit(x_train, y_train.values.ravel())
 
     y_pred = model.predict(x_test)
@@ -52,7 +53,7 @@ y_test_dr_pca = pd.read_csv('./files/y_test_pca_reduced.csv')
 def make_and_save_decision_tree_model_with_dimred_pca():
     dt = GridSearchCV(DecisionTreeRegressor(),
                       {'max_depth': [4, 5, 6, 7], 'min_samples_leaf': [0.05, 0.1, 0.15, 0.2, 0.3]}, cv=5,
-                      return_train_score=False)
+                      return_train_score=True)
     dt.fit(x_train_dr_pca, y_train_dr_pca.values.ravel())
     y_pred = dt.predict(x_test_dr_pca)
 
@@ -70,7 +71,7 @@ def make_and_save_decision_tree_model_with_dimred_pca():
     pd.DataFrame(dt.cv_results_).to_pickle('./decision_tree_model_pca_reduced.pkl')
 
 def make_svr_model_with_dimred_pca():
-    model = GridSearchCV(SVC(), {'kernel': ['linear', 'poly', 'rbf', 'sigmoid'], 'C': [1,2,3,4,5,6,7,8,9,10]}, cv=5, return_train_score=False)
+    model = GridSearchCV(SVC(), {'kernel': ['linear', 'poly', 'rbf', 'sigmoid'], 'C': [1,2,3,4,5,6,7,8,9,10]}, cv=5, return_train_score=True)
     model.fit(x_train_dr_pca, y_train_dr_pca.values.ravel())
 
     y_pred = model.predict(x_test_dr_pca)
@@ -94,7 +95,7 @@ y_test_dr_svd = pd.read_csv('./files/y_test_svd_reduced.csv')
 def make_and_save_decision_tree_model_with_dimred_svd():
     dt = GridSearchCV(DecisionTreeRegressor(),
                       {'max_depth': [4, 5, 6, 7], 'min_samples_leaf': [0.05, 0.1, 0.15, 0.2, 0.3]}, cv=5,
-                      return_train_score=False)
+                      return_train_score=True)
     dt.fit(x_train_dr_svd, y_train_dr_svd.values.ravel())
     y_pred = dt.predict(x_test_dr_svd)
 
@@ -111,8 +112,30 @@ def make_and_save_decision_tree_model_with_dimred_svd():
 
     pd.DataFrame(dt.cv_results_).to_pickle('./decision_tree_model_svd_reduced.pkl')
 
+def make_and_save_decision_tree_model_with_dimred_svd():
+        dt = GridSearchCV(DecisionTreeRegressor(),
+                          {'max_depth': [4, 5, 6, 7],
+                           'min_samples_leaf': [0.0001, 0.001, 0.01, 0.05, 0.1, 0.15, 0.2, 0.3]}, cv=5,
+                          return_train_score=True)
+        dt.fit(x_train_dr_svd, y_train_dr_svd.values.ravel())
+        y_pred = dt.predict(x_test_dr_svd)
+
+        mse_dt = MSE(y_test_dr_svd, y_pred)
+        rmse_dt = mse_dt ** (1 / 2)
+
+        print('mse:', mse_dt)
+        print('rmse:', rmse_dt)
+        print('best score:', dt.best_score_)
+        print('best param:', dt.best_params_)
+
+        df = pd.DataFrame(dt.cv_results_)
+        print(df)
+
+        pd.DataFrame(dt.cv_results_).to_pickle('./decision_tree_model_svd_reduced_higher.pkl')
+
+
 def make_svr_model_with_dimred_svd():
-    model = GridSearchCV(SVC(), {'kernel': ['linear', 'poly', 'rbf', 'sigmoid'], 'C': [1,2,3,4,5,6,7,8,9,10]}, cv=5, return_train_score=False)
+    model = GridSearchCV(SVC(), {'kernel': ['linear', 'poly', 'rbf', 'sigmoid'], 'C': [1,2,3,4,5,6,7,8,9,10]}, cv=5, return_train_score=True)
     model.fit(x_train_dr_svd, y_train_dr_svd.values.ravel())
 
     y_pred = model.predict(x_test_dr_svd)
@@ -126,6 +149,13 @@ def make_svr_model_with_dimred_svd():
     model.score(x_test_dr_svd, y_test_dr_svd)
 
     pd.DataFrame(model.cv_results_).to_pickle('./svr_model_svd_reduced.pkl')
+
+def teszt():
+    load = pickle.load('./svr_model.pkl')
+    df = pd.DataFrame(load)
+    print(df.head(5))
+
+teszt()
 
 make_and_save_decision_tree_model()
 make_and_save_decision_tree_model_with_dimred_pca()
